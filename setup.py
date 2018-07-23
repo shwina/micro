bf8fd8304c8f4395ea1ed05ec7e70fcb10d96dff
Ashwin Srinath <atrikut@clemson.edu>
2018-07-23 11:50:41 -0400

    Remove everything from setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,1 @@
-from setuptools import setup
-
-setup(name='micro',
-    version='0.1',
-    description='Tools for analysis of microscopic data',
-    url='github.com/shwina/micro',
-    author='Ashwin Srinath',
-    license='MIT',
-    packages=['micro'],
-    zip_safe=False)
+# setup.py
