57b43a0bb2e65e46b0f20feadac9b88ed667f575
Ashwin Srinath <atrikut@clemson.edu>
2018-07-23 12:17:34 -0400

    Add tests and setup.py to ndimg branch

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
-# setup.py
+from setuptools import setup
+
+setup(name='micro',
+    version='0.1',
+    description='Tools for analysis of microscopic data',
+    url='github.com/shwina/micro',
+    author='Ashwin Srinath',
+    license='MIT',
+    packages=['micro'],
+    zip_safe=False)
